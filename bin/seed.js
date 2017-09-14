8520ef75f27b8eff10fc125931ec6db64ca12482
Dharma Velasco <dharma.giselle@gmail.com>
2017-09-14 12:33:08 -0400

    Add example seed file

---
+++ bin/seed.js
@@ -0,0 +1,12 @@
+const mongoose     = require('mongoose');
+
+//load environment variables from ".env" file (put this at the top)
+require('dotenv').config();
+
+
+// 'MONGODB_URI' is defined in the '.env' file
+mongoose.connect(process.env.MONGODB_URI);
+
+
+//Connect to the database with the "MONGODB_URI" environment variable
+console.log('Example seed file running 🤑');
